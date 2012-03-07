ec56be7baec40b6dad8db2088bb907947a375af8
Jaime Iniesta <jaimeiniesta@gmail.com>
2012-03-07 12:23:25 +0100

    Use before filter to authenticate all journal_entries actions

--- client.rb
+++ client.rb
@@ -20,6 +20,14 @@ helpers do
       :name       => "#{session[:user_first_name]} #{session[:user_last_name]}"
     }
   end
+
+  def authenticate!
+    redirect to("/login") if !logged_in?
+  end
+end
+
+before '/journal_entries*' do
+  authenticate!
 end
 
 get '/' do
@@ -62,45 +70,33 @@ get '/logout' do
 end
 
 get '/journal_entries' do
-  if !logged_in?
+  begin
+    api_query = open("http://localhost:3000/api/v1/journal_entries", :http_basic_authentication => [current_user[:email], current_user[:password]])
+  rescue OpenURI::HTTPError => ex
+    logger.error "Exception at /journal_entries: #{ex.to_s}"
     redirect to("/login")
-  else
-    begin
-      api_query = open("http://localhost:3000/api/v1/journal_entries", :http_basic_authentication => [current_user[:email], current_user[:password]])
-    rescue OpenURI::HTTPError => ex
-      logger.error "Exception at /journal_entries: #{ex.to_s}"
-      redirect to("/login")
-    end
+  end
 
-    if api_query.status[0] == "200"
-      json_response = ActiveSupport::JSON.decode(api_query.read)
-      @journal_entries = json_response.sort_by {|entry| Time.parse(entry['date'])}.reverse
-      erb :journal_entries
-    else
-      redirect to("/login")
-    end
+  if api_query.status[0] == "200"
+    json_response = ActiveSupport::JSON.decode(api_query.read)
+    @journal_entries = json_response.sort_by {|entry| Time.parse(entry['date'])}.reverse
+    erb :journal_entries
+  else
+    redirect to("/login")
   end
 end
 
 post '/journal_entries' do
-  if !logged_in?
-    redirect to("/login")
-  else
-    begin
-      resource = RestClient::Resource.new('http://localhost:3000/api/v1/journal_entries', :user => current_user[:email], :password => current_user[:password])
-      resource.post(:journal_entry => {:body => params[:journal_entry_body], :private => params[:journal_entry_private], :date => Time.now})
-    rescue => ex
-      logger.error "Exception at /journal_entries: #{ex.to_s}"
-    end
-
-    redirect to("/journal_entries")
+  begin
+    resource = RestClient::Resource.new('http://localhost:3000/api/v1/journal_entries', :user => current_user[:email], :password => current_user[:password])
+    resource.post(:journal_entry => {:body => params[:journal_entry_body], :private => params[:journal_entry_private], :date => Time.now})
+  rescue => ex
+    logger.error "Exception at /journal_entries: #{ex.to_s}"
   end
+
+  redirect to("/journal_entries")
 end
 
 get '/journal_entries/new' do
-  if !logged_in?
-    redirect to("/login")
-  else
-    erb :journal_entries_new
-  end
+  erb :journal_entries_new
 end
